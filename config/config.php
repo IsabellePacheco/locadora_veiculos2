9841f4bdde39f473b86bd8667b63765c876eb37c
IsabellePacheco <isapachecosenai@gmail.com>
2025-04-28 10:28:59 -0300

    config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// arquivo de configiração com as constantes do sistema
+
+define('ARQUIVO_JSON', __DIR__. '/../data/veiculos.json');
+define('ARQUIVO_USUARIOS', __DIR__. '/../data/usuarios.json');
+define('DIARIA_CARRO', 200.00);
+define('DIARIA_MOTO', 150.00);
