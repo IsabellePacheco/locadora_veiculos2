bf18073c5c774d4623672d45b8ced80c12c793ea
IsabellePacheco <isapachecosenai@gmail.com>
2025-04-28 09:55:18 -0300

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'locadora/veiculos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da73375901a531eb0a2baec7f71eb12157cc65d2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'locadora/veiculos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da73375901a531eb0a2baec7f71eb12157cc65d2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
