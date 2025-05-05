037c4af7168c6d01331b0dc14e09ed7f89dcd3cb
IsabellePacheco <isapachecosenai@gmail.com>
2025-05-05 08:41:36 -0300

    config

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
+// Arquivo de configuração com as constantes do sistema
 
-// arquivo de configiração com as constantes do sistema
+// Criando variável "ARQUIVO JSON" que envia para outro diretório
+define('ARQUIVO_JSON', __DIR__ . '/../data/veiculos.json');
+define('ARQUIVO_USUARIOS', __DIR__ . '/../data/usuarios.json');
+define('DIARIA_CARRO', 100.00);
+define('DIARIA_MOTO', 50.00);
 
-define('ARQUIVO_JSON', __DIR__. '/../data/veiculos.json');
-define('ARQUIVO_USUARIOS', __DIR__. '/../data/usuarios.json');
-define('DIARIA_CARRO', 200.00);
-define('DIARIA_MOTO', 150.00);
+?>
